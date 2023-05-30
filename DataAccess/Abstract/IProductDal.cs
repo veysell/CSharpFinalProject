266c33f5ee2b32c920587425f5165d24d4378f6d
Veysel Duran <vdrn76@gmail.com>
2023-05-31 01:30:17 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        List<Product> GetAll();
+        
+        void Add();
+
+        void Update();
+
+        void Delete();
+
+        List<Product> GetAllByCategory(int categoryId); 
+    }
+}
