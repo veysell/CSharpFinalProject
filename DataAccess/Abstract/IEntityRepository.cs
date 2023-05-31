ab5980acf78fbf3a50b1b93e8f99c2fed2add600
Veysel Duran <vdrn76@gmail.com>
2023-06-01 00:15:36 +0300

    EntityFramework added, Database added

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class : referans tip
+    //new() : newlenebilir olmalı 
+    public interface IEntityRepository<T> where T:class,IEntity,new()   
+    {
+        //filtreler yazmayı sağlayan Delege
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        
+        T Get(Expression<Func<T, bool>> filter);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NortwindContext.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //proje classları ile db tablolarını bağlama
+    public class NortwindContext:DbContext 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-B4T1LFKH;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+        
+        public DbSet<Customer> Customers { get; set; }
+
+
+    }
+}
