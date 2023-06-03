d5916ebfa77d2b2df26f139d74245e41a9336a6c
Veysel Duran <vdrn76@gmail.com>
2023-06-03 22:08:30 +0300

    autofac,validate,cross cutter concerns

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,12 +1,16 @@
 ﻿using Business.Abstract;
 using Business.Constans;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,11 +28,9 @@ namespace Business.Concrete
 
         public IResult Add(Product product)
         {
-            //businnes codes
-            if (product.ProductName.Length < 2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+           
+            ValidationTool.Validate(new ProductValidator(), product);
+
             _productDal.Add(product);
             return new Result(true, Messages.ProductAdded);
         }
