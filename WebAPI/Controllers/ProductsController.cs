bc53958b804c6de1798fdfacf1683548754ed8b1
Veysel Duran <vdrn76@gmail.com>
2023-06-07 23:15:33 +0300

    category controller added

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -26,6 +26,8 @@ namespace WebAPI.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
+            System.Threading.Thread.Sleep(2000);
+
             var result=_productService.GetAll();
             if (result.Success)
             {
